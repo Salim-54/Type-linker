590f8465196a3bb2d54034458f90ab7edc2d219f
NDAYISABYE Salim <ndayisabyesalim54@gmail.com>
2022-07-24 17:02:27 +0200

    ft(filtering): initiated

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,12 +1,31 @@
-import { objectType, extendType, nonNull, stringArg } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";
+import { extendType, nonNull, objectType, stringArg, intArg, inputObjectType, enumType, arg, list } from "nexus";
+import { Prisma } from "@prisma/client";
+
+
+
+
+export const LinkOrderByInput = inputObjectType({
+    name: "LinkOrderByInput",
+    definition(t) {
+        t.field("description", { type: Sort });
+        t.field("url", { type: Sort });
+        t.field("createdAt", { type: Sort });
+    },
+});
+
+export const Sort = enumType({
+    name: "Sort",
+    members: ["asc", "desc"],
+});
 
 export const Link = objectType({
     name: "Link", // 1 
     definition(t) {  // 2
         t.nonNull.int("id"); // 3 
         t.nonNull.string("description"); // 4
-        t.nonNull.string("url");
+        t.nonNull.string("url")
+        t.nonNull.dateTime("createdAt");  
         t.field("postedBy", {   // 1
             type: "User",
             resolve(parent, args, context) {  // 2
@@ -14,22 +33,60 @@ export const Link = objectType({
                     .findUnique({ where: { id: parent.id } })
                     .postedBy();
             },
-        });// 5 
-    },
-});
-
-export const LinkQuery = extendType({  // 2
-    type: "Query",
-    definition(t) {
-        t.nonNull.list.nonNull.field("feed", {   // 3
-            type: "Link",
-            resolve(parent, args, context, info) {    // 4
-                return context.prisma.link.findMany();;
-            },
+        });
+        t.nonNull.list.nonNull.field("voters", {  // 1
+            type: "User",
+            resolve(parent, args, context) {
+                return context.prisma.link
+                    .findUnique({ where: { id: parent.id } })
+                    .voters();
+            }
         });
     },
 });
 
+export const LinkQuery = extendType({
+    type: "Query",
+    definition(t) {
+        t.nonNull.field("feed", {  // 1
+            type: "Feed",
+            args: {
+                filter: stringArg(),
+                skip: intArg(),
+                take: intArg(),
+                orderBy: arg({ type: list(nonNull(LinkOrderByInput)) }), 
+            },
+            async resolve(parent, args, context) {  
+                const where = args.filter
+                    ? {
+                          OR: [
+                              { description: { contains: args.filter } },
+                              { url: { contains: args.filter } },
+                          ],
+                      }
+                    : {};
+
+                const links = await context.prisma.link.findMany({  
+                    where,
+                    skip: args?.skip as number | undefined,
+                    take: args?.take as number | undefined,
+                    orderBy: args?.orderBy as
+                        | Prisma.Enumerable<Prisma.LinkOrderByWithRelationInput>
+                        | undefined,
+                });
+
+                const count = await context.prisma.link.count({ where });  // 2
+                const id = `main-feed:${JSON.stringify(args)}`;  // 3
+                  
+                return {  // 4
+                    links,
+                    count,
+                    id,
+                };
+            },
+        });
+    },
+});
 
 export const LinkMutation = extendType({  // 1
     type: "Mutation",
@@ -57,8 +114,18 @@ export const LinkMutation = extendType({  // 1
                     },
                 });
 
+
                 return newLink;
             },
         });
+    },
+});
+
+export const Feed = objectType({
+    name: "Feed",
+    definition(t) {
+        t.nonNull.list.nonNull.field("links", { type: Link }); // 1
+        t.nonNull.int("count"); // 2
+        t.id("id");  // 3
     },
 });
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,3 +1,5 @@
 export * from "./User"; 
 export * from "./Link"; 
 export * from "./Auth"; 
+export * from "./Vote"; 
+export * from "./scalars/Date";
